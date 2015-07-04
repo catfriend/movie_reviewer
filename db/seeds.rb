1003c63dc7adfaff1c348726452a8f9bc8f563d5
catfriend <eakari@live.com>
2015-07-04 11:58:14 -0700

    finished adding basic CRUD routes and tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Movie.create!([
+  {
+    title: "Iron Man",
+    rating: "PG-13",
+    total_gross: 318412101.00,
+    description: "Tony Stark builds himself a nifty armored suit to fight naturally gifted evil types.",
+    released_on: "2008-05-02"
+  },
+  {
+    title: "Superman",
+    rating: "PG",
+    total_gross: 134218018.00,
+    description: "An orphan from Krypton, Clark Kent grows up in the midwest, moves to the Big City, and decides to fight crime in his spare time.",
+    released_on: "1978-12-15"
+  },
+  {
+    title: "Spider-Man",
+    rating: "PG-13",
+    total_gross: 403706375.00,
+    description: "Peter Parker gets bit by a genetically modified spider, sews himself a spandex suit, and swings around town.",
+    released_on: "2002-05-03"
+  },
+  {
+    title: "Catwoman",
+    rating: "PG-13",
+    total_gross: 40200000.00,
+    description: "Patience Philips takes the expression crazy cat lady to heart.",
+    released_on: "2004-07-23"
+  }
+])
