e625750f9e4e06d84f3b5f84c9c9c2af6e4f7e21
catfriend <eakari@live.com>
2015-08-03 17:34:20 -0700

    allow signed in users to write reviews, associated with gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
-  def profile_image_for(user)
+  def profile_image_for(user, options={})
+    size = options[:size] || 80
     url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
     image_tag(url, alt: user.name)
   end
